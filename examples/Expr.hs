6793518a4de4706a25b05088779ca64e9fbd237b
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-12-10 17:19:52 +0200

    Use HUnit in Example

--- examples/Expr.hs
+++ examples/Expr.hs
@@ -4,6 +4,8 @@ module Main where
 import Data.Functor.Foldable
 import Data.Functor.Foldable.TH
 
+import Test.HUnit
+
 data Expr a
     = Lit a
     | Add (Expr a) (Expr a)
@@ -18,8 +20,8 @@ expr1 = Add (Lit 2) (Mul (Lit 3) (Lit 4))
 main :: IO ()
 main = do
     let expr2 = ana divCoalg 55 :: Expr Int
-    print $ cata evalAlg expr1
-    print $ cata evalAlg expr2
+    14 @=? cata evalAlg expr1
+    55 @=? cata evalAlg expr2
 
     -- assert expr1 evaluates to 14
     -- assert expr2 evaluates to 55, QuickCheck ?
