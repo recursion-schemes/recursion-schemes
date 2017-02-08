8a3f0728f719da96fbc518e133c2f0a00dfbf5ba
Nathan van Doorn <nvd1234@gmail.com>
2017-02-08 11:52:06 +0000

    Use nats library on older GHC

--- Data/Functor/Foldable.hs
+++ Data/Functor/Foldable.hs
@@ -12,8 +12,6 @@
 -- Polymorphic typeable
 #define HAS_POLY_TYPEABLE MIN_VERSION_base(4,7,0)
 
-#define HAS_NATURAL (__GLASGOW_HASKELL__ >= 710)
-
 #ifdef __GLASGOW_HASKELL__
 {-# LANGUAGE DeriveDataTypeable #-}
 #if __GLASGOW_HASKELL__ >= 800
@@ -130,9 +128,7 @@ import GHC.Generics (Generic)
 import GHC.Generics (Generic1)
 #endif
 #endif
-#if HAS_NATURAL
 import Numeric.Natural
-#endif
 import Data.Monoid (Monoid (..))
 import Prelude
 
@@ -339,14 +335,12 @@ instance Recursive (NonEmpty a) where
 instance Corecursive (NonEmpty a) where
   embed = (:|) <$> NEF.head <*> (maybe [] toList <$> NEF.tail)
 
-#if HAS_NATURAL
 type instance Base Natural = Maybe
 instance Recursive Natural where
   project 0 = Nothing
   project n = Just (n - 1)
 instance Corecursive Natural where
   embed = maybe 0 (+1)
-#endif
 
 -- | Cofree comonads are Recursive/Corecursive
 type instance Base (Cofree f a) = CofreeF f a
